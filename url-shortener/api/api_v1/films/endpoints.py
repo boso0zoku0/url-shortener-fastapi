a5293bf7e16633a245a5708d71dfad7f954739d7
BOSOZOKU <matveygrishayev@mail.ru>
2025-07-06 17:18:18 +0300

    update endpoint search/slug, create/films

--- url-shortener/api/api_v1/films/endpoints.py
+++ url-shortener/api/api_v1/films/endpoints.py
@@ -1,30 +1,30 @@
 from fastapi import APIRouter, HTTPException, status
-from schemas.films import Films
-from .crud import FILMS
+from schemas.films import *
+from .crud import storage
 
 router = APIRouter(prefix="/films", tags=["Films"])
 
 
-@router.get("/", response_model=list[Films])
-def show_firms():
-    return FILMS
+#
+@router.get("/", response_model=list[FilmsGet])
+def show_films():
+    return storage.get_films()
 
 
-@router.get("/{name}", response_model=Films)
-def show_film_by_name(name: str):
-    film: Films = next((film for film in FILMS if film.name == name), None)
-    if film:
-        return film
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND, detail=f"Film {name!r} not found"
-    )
+@router.post(
+    "/create/films/",
+    response_model=FilmsCreate | None,
+    status_code=status.HTTP_201_CREATED,
+)
+def create_film(film_create: FilmsCreate):
+    return storage.create_film(create_films=film_create)
 
 
-@router.get("/{slug}", response_model=Films)
-def show_film_by_slug(slug: str):
-    film: Films = next((film for film in FILMS if film.slug == slug), None)
-    if film:
-        return film
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND, detail=f"Film {slug!r} not found"
-    )
+@router.get("/search/{slug}", response_model=FilmsGet)
+def search_film_by_slug(slug: str):
+    get_film = storage.get_by_slug_film(slug=slug)
+    if get_film is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"Film {slug} not found"
+        )
+    return get_film
