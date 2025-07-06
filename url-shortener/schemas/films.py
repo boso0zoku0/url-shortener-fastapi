ea7417392d365fb6d7c75ac88c144ec015cd3a9a
BOSOZOKU <matveygrishayev@mail.ru>
2025-07-06 17:02:34 +0300

    changed the schemes

--- url-shortener/schemas/films.py
+++ url-shortener/schemas/films.py
@@ -1,11 +1,18 @@
 from pydantic import BaseModel
+from typing import Annotated
+from annotated_types import Len
 
 
 class FilmsBase(BaseModel):
     name: str
     description: str
     year_release: int
-
-
-class Films(FilmsBase):
     slug: str
+
+
+class FilmsGet(FilmsBase):
+    pass
+
+
+class FilmsCreate(FilmsGet):
+    slug: Annotated[str, Len(min_length=3, max_length=10)]
