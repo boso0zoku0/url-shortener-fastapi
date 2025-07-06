2ddf1d6811cab25074bd9d0e499972e4a5c1206e
BOSOZOKU <matveygrishayev@mail.ru>
2025-07-06 17:17:42 +0300

    create FilmsStorage

--- url-shortener/api/api_v1/films/crud.py
+++ url-shortener/api/api_v1/films/crud.py
@@ -1,22 +1,47 @@
-from schemas.films import Films
+from schemas.films import *
+from pydantic import BaseModel
 
-FILMS = [
-    Films(
-        slug="Интер",
+
+class FilmsStorage(BaseModel):
+    slug_by_films: dict[str, FilmsGet] = {}
+
+    def get_films(self) -> list[FilmsGet]:
+        return list(self.slug_by_films.values())
+
+    def get_by_slug_film(self, slug: str) -> FilmsGet | None:
+        return self.slug_by_films.get(slug)
+
+    def create_film(self, create_films: FilmsCreate) -> FilmsGet:
+        new_film = FilmsGet(**create_films.model_dump())
+        self.slug_by_films[new_film.slug] = new_film
+        return new_film
+
+
+storage = FilmsStorage()
+
+storage.create_film(
+    FilmsCreate(
         name="Криминальное чтиво",
         description="Нелинейная криминальная драма",
         year_release=1994,
-    ),
-    Films(
-        slug="Интер",
+        slug="Чтиво",
+    )
+)
+
+storage.create_film(
+    FilmsCreate(
         name="Интерстеллар",
         description="Космическая одиссея о спасении человечества",
         year_release=2014,
-    ),
-    Films(
-        slug="МТЦ",
+        slug="Интер",
+    )
+)
+
+storage.create_film(
+    FilmsCreate(
         name="Матрица",
         description="Реальность иллюзия виртуального мира",
         year_release=1999,
-    ),
-]
+        slug="МТЦ",
+    )
+)
