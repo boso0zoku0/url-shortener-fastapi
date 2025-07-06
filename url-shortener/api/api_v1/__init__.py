6cf2b6eb4bc33b40d2ff20017ab3540dc7f6c121
BOSOZOKU <matveygrishayev@mail.ru>
2025-07-06 18:49:50 +0300

    cnahged views/endpoints routers

--- url-shortener/api/api_v1/__init__.py
+++ url-shortener/api/api_v1/__init__.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from api.api_v1.short_urls.views import router as short_url_router
+from api.api_v1.short_urls.endpoints import router as short_url_router
 from api.api_v1.short_urls.redirect_views import router as redirect_router
 
 router = APIRouter(prefix="/v1")
