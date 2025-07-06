54c79d2ad4163060de1056b8293609e057b6034d
BOSOZOKU <matveygrishayev@mail.ru>
2025-07-06 12:40:05 +0300

    moved two files in short_urls

--- url-shortener/api/api_v1/short_urls/crud.py
+++ url-shortener/api/api_v1/short_urls/crud.py
@@ -1,6 +1,6 @@
 from schemas.short_url import ShortUrl
 
 SHORT_URLS = [
-    ShortUrl(target_url="http://google.com", slug="google"),
-    ShortUrl(target_url="http://yandex.ru", slug="yandex"),
+    ShortUrl(id=1, target_url="http://google.com", slug="google"),
+    ShortUrl(id=2, target_url="http://yandex.ru", slug="yandex"),
 ]
--- url-shortener/api/api_v1/short_urls/redirect_views.py
+++ url-shortener/api/api_v1/short_urls/redirect_views.py
@@ -1,9 +1,8 @@
 from fastapi import APIRouter
 from typing import Annotated
-from fastapi import HTTPException, Depends, status, Request
-from starlette.responses import RedirectResponse
+from fastapi import Depends
 from schemas.short_url import ShortUrl
-from .short_urls.dependencies import prefetch_url
+from api.api_v1.short_urls.dependencies import prefetch_url
 
 router = APIRouter(prefix="/r", tags=["Redirect"])
 
