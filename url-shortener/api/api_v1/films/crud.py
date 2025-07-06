1abfccbd5dac25968862b18126377740a66256fd
BOSOZOKU <matveygrishayev@mail.ru>
2025-07-06 11:40:08 +0300

    add FILMS storage

---
+++ url-shortener/api/api_v1/films/crud.py
@@ -0,0 +1,22 @@
+from schemas.films import Films
+
+FILMS = [
+    Films(
+        id=1,
+        name="Криминальное чтиво",
+        description="Нелинейная криминальная драма",
+        year_release=1994,
+    ),
+    Films(
+        id=2,
+        name="Интерстеллар",
+        description="Космическая одиссея о спасении человечества",
+        year_release=2014,
+    ),
+    Films(
+        id=3,
+        name="Матрица",
+        description="Реальность иллюзия виртуального мира",
+        year_release=1999,
+    ),
+]
