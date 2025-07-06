67901cb54c4cb09ff645b14d08eed11bff5d9779
BOSOZOKU <matveygrishayev@mail.ru>
2025-07-06 13:20:40 +0300

    changed id to slug

--- url-shortener/api/api_v1/films/crud.py
+++ url-shortener/api/api_v1/films/crud.py
@@ -2,19 +2,19 @@ from schemas.films import Films
 
 FILMS = [
     Films(
-        id=1,
+        slug="Интер",
         name="Криминальное чтиво",
         description="Нелинейная криминальная драма",
         year_release=1994,
     ),
     Films(
-        id=2,
+        slug="Интер",
         name="Интерстеллар",
         description="Космическая одиссея о спасении человечества",
         year_release=2014,
     ),
     Films(
-        id=3,
+        slug="МТЦ",
         name="Матрица",
         description="Реальность иллюзия виртуального мира",
         year_release=1999,
