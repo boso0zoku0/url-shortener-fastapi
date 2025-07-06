cf8589d9e53336a66fedab57017ce6d9368b8e01
BOSOZOKU <matveygrishayev@mail.ru>
2025-07-06 17:18:43 +0300

    new version view

--- url-shortener/api/api_v1/short_urls/views.py
+++ url-shortener/api/api_v1/short_urls/views.py
@@ -1,30 +1,22 @@
-from fastapi import HTTPException, status, Request
-from rich.json import JSON
+from fastapi import status
 
 from schemas.short_url import *
 from fastapi import APIRouter
-import random
-from api.api_v1.short_urls.crud import SHORT_URLS
+from .crud import storage
 
 router = APIRouter(prefix="/short-urls", tags=["Short URLs"])
 
 
-@router.get("/")
-def root(request: Request):
-    data = request.url.replace(path="/docs", query="")
-    return {"Hello": "Y", "data": data}
+@router.get("/", response_model=list[ShortUrl])
+def read_short_urls_list():
+    return storage.get()
 
 
-@router.post("/", response_model=ShortUrl, status_code=status.HTTP_201_CREATED)
-def create_short_url(short_url: Annotated[ShortUrlCreate, JSON]):
-    return ShortUrl(id=random.randint(0, 10), **short_url.model_dump())
+@router.post("/create/", response_model=ShortUrl, status_code=status.HTTP_201_CREATED)
+def create_short_url(short_url: ShortUrlCreate):
+    return storage.create(short_url)
 
 
 @router.get("/{slug}/search")
 def search_url(slug: str):
-    url: ShortUrl = next((url for url in SHORT_URLS if url.slug == slug), None)
-    if url:
-        return url
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND, detail=f"URL {slug!r} not found"
-    )
+    return storage.get_by_slug(slug=slug)
