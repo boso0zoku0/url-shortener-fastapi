60d7f2c5db31042b822a3f29eee3eeb01b602410
BOSOZOKU <matveygrishayev@mail.ru>
2025-07-06 11:39:28 +0300

    add SHORT_URLS storage

---
+++ url-shortener/api/api_v1/short_urls/crud.py
@@ -0,0 +1,6 @@
+from schemas.short_url import ShortUrl
+
+SHORT_URLS = [
+    ShortUrl(target_url="http://google.com", slug="google"),
+    ShortUrl(target_url="http://yandex.ru", slug="yandex"),
+]
