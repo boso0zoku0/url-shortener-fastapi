ef56f4232d78b3af29ec1812cb2f230bdf3b1d05
BOSOZOKU <matveygrishayev@mail.ru>
2025-07-06 17:03:26 +0300

    create short_urls_storage

--- url-shortener/api/api_v1/short_urls/crud.py
+++ url-shortener/api/api_v1/short_urls/crud.py
@@ -1,7 +1,36 @@
-from schemas.short_url import ShortUrl
-import random
+from pydantic import BaseModel, AnyHttpUrl
 
-SHORT_URLS = [
-    ShortUrl(id=random.randint(0, 10), target_url="http://google.com", slug="google"),
-    ShortUrl(id=random.randint(0, 10), target_url="http://yandex.ru", slug="yandex"),
-]
+from schemas.short_url import ShortUrl, ShortUrlCreate
+
+
+class ShortUrlsStorage(BaseModel):
+    slug_by_short_urls: dict[str, ShortUrl] = {}
+
+    def get(self) -> list[ShortUrl]:
+        return list(self.slug_by_short_urls.values())
+
+    def get_by_slug(self, slug: str) -> ShortUrl | None:
+        return self.slug_by_short_urls.get(slug)
+
+    def create(self, short_url_create: ShortUrlCreate) -> ShortUrl:
+        short_url = ShortUrl(**short_url_create.model_dump())
+        self.slug_by_short_urls[short_url.slug] = (
+            short_url  # обращаемся к slug_by_short_urls и по slug-у short_url сохраняем short_url в словарь
+        )
+        return short_url
+
+
+storage = ShortUrlsStorage()
+
+storage.create(
+    ShortUrlCreate(
+        target_url=AnyHttpUrl("https://www.example.com"),
+        slug="example",
+    ),
+)
+storage.create(
+    ShortUrlCreate(
+        target_url=AnyHttpUrl("https://www.google.com"),
+        slug="search",
+    ),
+)
