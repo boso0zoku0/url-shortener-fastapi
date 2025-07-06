d95bee6c4edd966a9345402581d00937c9fb2cef
BOSOZOKU <matveygrishayev@mail.ru>
2025-07-06 13:21:10 +0300

    change endpoint id to slug

--- url-shortener/api/api_v1/films/endpoints.py
+++ url-shortener/api/api_v1/films/endpoints.py
@@ -10,21 +10,21 @@ def show_firms():
     return FILMS
 
 
-@router.get("/{idx}", response_model=Films)
-def show_film_by_id(idx: int):
-    film: Films = next((film for film in FILMS if film.id == idx), None)
-    if film:
-        return film
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND, detail=f"Film {id!r} not found"
-    )
-
-
 @router.get("/{name}", response_model=Films)
 def show_film_by_name(name: str):
     film: Films = next((film for film in FILMS if film.name == name), None)
     if film:
         return film
     raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND, detail=f"Film {name} not found"
+        status_code=status.HTTP_404_NOT_FOUND, detail=f"Film {name!r} not found"
+    )
+
+
+@router.get("/{slug}", response_model=Films)
+def show_film_by_slug(slug: str):
+    film: Films = next((film for film in FILMS if film.slug == slug), None)
+    if film:
+        return film
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, detail=f"Film {slug!r} not found"
     )
