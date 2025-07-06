0eee343245c88f39774ae26d9cc2db3124d0ba2b
BOSOZOKU <matveygrishayev@mail.ru>
2025-07-06 18:53:40 +0300

    change imports

--- url-shortener/api/api_v1/short_urls/redirect_views.py
+++ url-shortener/api/api_v1/short_urls/redirect_views.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from typing import Annotated
 from fastapi import Depends
 from schemas.short_url import ShortUrl
-from api.api_v1.short_urls.dependencies import prefetch_url
+from api.api_v1.dependencies import prefetch_url
 
 router = APIRouter(prefix="/r", tags=["Redirect"])
 
