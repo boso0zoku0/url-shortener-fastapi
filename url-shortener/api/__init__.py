c8bf527d27574202c9fc1ba26808947fb3aff8fd
BOSOZOKU <matveygrishayev@mail.ru>
2025-07-07 16:53:39 +0300

    updated imports

--- url-shortener/api/__init__.py
+++ url-shortener/api/__init__.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 from .api_v1 import router as api_v1_router
-from .api_v1.films.endpoints import router as films_router
+
+# from .api_v1.films.endpoints import router as films_router
 
 router = APIRouter(prefix="/api")
 router.include_router(router=api_v1_router)
-router.include_router(router=films_router)
+# router.include_router(router=films_router)
