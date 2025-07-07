0983ce2c000da9a20a81dece94700d57ad445178
BOSOZOKU <matveygrishayev@mail.ru>
2025-07-07 19:38:49 +0300

    change endpoints by used ShortUrlRead

--- url-shortener/api/api_v1/short_urls/views/list_router.py
+++ url-shortener/api/api_v1/short_urls/views/list_router.py
@@ -2,21 +2,23 @@ from fastapi import APIRouter
 from starlette import status
 
 from api.api_v1.short_urls.crud import storage_short_urls
-from schemas.short_url import ShortUrl, ShortUrlCreate
+from schemas.short_url import ShortUrl, ShortUrlCreate, ShortUrlRead
 
 router = APIRouter(prefix="/short-urls", tags=["Short URLs"])
 
 
-@router.get("/", response_model=list[ShortUrl])
-def read_short_urls_list():
+@router.get("/", response_model=list[ShortUrlRead])
+def read_short_urls_list() -> list[ShortUrl]:
     return storage_short_urls.get()
 
 
-@router.get("/search")
+@router.get("/search", response_model=ShortUrlRead)
 def search_url(slug: str):
     return storage_short_urls.get_by_slug(slug=slug)
 
 
-@router.post("/create/", response_model=ShortUrl, status_code=status.HTTP_201_CREATED)
+@router.post(
+    "/create/", response_model=ShortUrlRead, status_code=status.HTTP_201_CREATED
+)
 def create_short_url(short_url: ShortUrlCreate):
     return storage_short_urls.create(short_url)
