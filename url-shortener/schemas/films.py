2a09aa3e7420436ee37c5750b045586055d7d92e
BOSOZOKU <matveygrishayev@mail.ru>
2025-07-07 18:27:13 +0300

    add FilmsUpdate schema

--- url-shortener/schemas/films.py
+++ url-shortener/schemas/films.py
@@ -1,11 +1,11 @@
 from pydantic import BaseModel
 from typing import Annotated
-from annotated_types import Len
+from annotated_types import Len, MaxLen
 
 
 class FilmsBase(BaseModel):
     name: str
-    description: str
+    description: Annotated[str, MaxLen(50)] | None = ""
     year_release: int
     slug: str
 
@@ -15,4 +15,8 @@ class FilmsGet(FilmsBase):
 
 
 class FilmsCreate(FilmsGet):
-    slug: Annotated[str, Len(min_length=3, max_length=10)]
+    slug: Annotated[str, Len(min_length=3, max_length=50)]
+
+
+class FilmsUpdate(FilmsGet):
+    description: Annotated[str, MaxLen(50)]
