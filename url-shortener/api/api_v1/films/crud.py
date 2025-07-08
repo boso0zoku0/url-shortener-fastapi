2454f4bb6be7463d40227d62f6bf66df37362c60
BOSOZOKU <matveygrishayev@mail.ru>
2025-07-08 18:20:29 +0300

    change update_partial

--- url-shortener/api/api_v1/films/crud.py
+++ url-shortener/api/api_v1/films/crud.py
@@ -3,33 +3,33 @@ from pydantic import BaseModel
 
 
 class FilmsStorage(BaseModel):
-    slug_by_films: dict[str, FilmsGet] = {}
+    slug_by_films: dict[str, FilmsRead] = {}
 
-    def get_films(self) -> list[FilmsGet]:
+    def get_films(self) -> list[FilmsRead]:
         return list(self.slug_by_films.values())
 
-    def get_by_slug(self, slug: str) -> FilmsGet | None:
+    def get_by_slug(self, slug: str) -> FilmsRead | None:
         return self.slug_by_films.get(slug)
 
-    def create_film(self, create_films: FilmsCreate) -> FilmsGet:
-        new_film = FilmsGet(**create_films.model_dump())
+    def create_film(self, create_films: FilmsCreate) -> FilmsRead:
+        new_film = FilmsRead(**create_films.model_dump())
         self.slug_by_films[new_film.slug] = new_film
         return new_film
 
     def delete_by_slug(self, slug: str) -> None:
         self.slug_by_films.pop(slug, None)
 
-    def delete(self, film_url: FilmsGet) -> None:
+    def delete(self, film_url: FilmsRead) -> None:
         return self.delete_by_slug(slug=film_url.slug)
 
-    def update(self, film: FilmsGet, film_update: FilmsUpdate) -> FilmsGet:
+    def update(self, film: FilmsRead, film_update: FilmsUpdate) -> FilmsRead:
         for k, v in film_update:
             setattr(film, k, v)
         return film
 
     def update_partial(
-        self, film: FilmsGet, film_update_partial: FilmsUpdatePartial
-    ) -> FilmsGet:
+        self, film: FilmsRead, film_update_partial: FilmsUpdatePartial
+    ) -> FilmsRead:
         for k, v in film_update_partial.model_dump(exclude_unset=True).items():
             setattr(film, k, v)
         return film
