febb015929676621934cf45da45b588b63105624
BOSOZOKU <matveygrishayev@mail.ru>
2025-07-09 18:12:38 +0300

    changed storage_films to storage

--- url-shortener/api/api_v1/films/views/details_router.py
+++ url-shortener/api/api_v1/films/views/details_router.py
@@ -4,7 +4,7 @@ from fastapi import Depends
 from starlette import status
 
 from api.api_v1.dependencies import prefetch_url_film, get_film_by_slug_exc
-from api.api_v1.films.crud import storage_films
+from api.api_v1.films.crud import storage
 from schemas.films import FilmsRead, FilmsUpdate, FilmsUpdatePartial
 from fastapi import APIRouter
 
@@ -28,7 +28,7 @@ FilmBySlug = Annotated[FilmsRead, Depends(prefetch_url_film)]
 
 
 @router.get("/", response_model=FilmsRead)
-def search_film_by_slug(slug=Depends(storage_films.get_by_slug)):
+def search_film_by_slug(slug=Depends(storage.get_by_slug)):
     return get_film_by_slug_exc(slug)
 
 
@@ -49,14 +49,14 @@ def search_film_by_slug(slug=Depends(storage_films.get_by_slug)):
     },
 )
 def delete_film(url: Annotated[FilmsRead, Depends(prefetch_url_film)]):
-    return storage_films.delete(film_url=url)
+    return storage.delete(film_url=url)
 
 
 @router.put("/", response_model=FilmsRead)
 def put_film(film: FilmBySlug, film_update: FilmsUpdate) -> FilmsRead:
-    return storage_films.update(film=film, film_update=film_update)
+    return storage.update(film=film, film_update=film_update)
 
 
 @router.patch("/", response_model=FilmsRead)
 def patch_film(film: FilmBySlug, film_update: FilmsUpdatePartial) -> FilmsRead:
-    return storage_films.update_partial(film=film, film_update_partial=film_update)
+    return storage.update_partial(film=film, film_update_partial=film_update)
