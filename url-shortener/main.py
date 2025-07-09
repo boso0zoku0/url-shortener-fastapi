83e3f83c579610d4cf60d4788b07ae6b02af173f
BOSOZOKU <matveygrishayev@mail.ru>
2025-07-09 18:13:25 +0300

    add logging config

--- url-shortener/main.py
+++ url-shortener/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI, status, HTTPException
 from schemas.films import *
 from api import router as api_router
+import logging
+from core import config
 
+log = logging.basicConfig(format=config.LOG_FORMAT, level=config.LOG_LEVEL)
 app = FastAPI()
 app.include_router(router=api_router)
