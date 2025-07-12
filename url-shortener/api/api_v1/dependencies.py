7cea3090bf5294f1eb3d1f54709a23ebdefb0e10
BOSOZOKU <matveygrishayev@mail.ru>
2025-07-12 16:08:09 +0300

    add func save_storage_state(BackgroundTasks)

--- url-shortener/api/api_v1/dependencies.py
+++ url-shortener/api/api_v1/dependencies.py
@@ -1,8 +1,11 @@
 from schemas.short_url import ShortUrl
 from schemas.films import FilmsRead
-from fastapi import HTTPException, status, Depends
+from fastapi import HTTPException, status, Depends, BackgroundTasks
 from api.api_v1.short_urls.crud import storage
 from api.api_v1.films.crud import storage as film_storage
+import logging
+
+log = logging.getLogger(__name__)
 
 
 def prefetch_url(slug: str):
@@ -28,3 +31,12 @@ def get_film_by_slug_exc(slug: str = Depends(film_storage.get_by_slug)):
     if slug is None:
         raise HTTPException(status_code=404, detail=f"Slug by Film: {slug} not found")
     return slug
+
+
+def save_storage_state(
+    background_tasks: BackgroundTasks,
+):
+
+    yield
+    log.info("Add background task to save storage")
+    background_tasks.add_task(storage.save_state)
