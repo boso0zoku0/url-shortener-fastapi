47b931ead28554565f19d792c68fdee914a8e19c
BOSOZOKU <matveygrishayev@mail.ru>
2025-07-12 13:58:08 +0300

    add BackgroundTasks

--- url-shortener/api/api_v1/films/views/list_router.py
+++ url-shortener/api/api_v1/films/views/list_router.py
@@ -3,8 +3,10 @@ from starlette import status
 from api.api_v1.films.crud import storage
 from schemas.films import FilmsRead, FilmsCreate
 
-from fastapi import APIRouter
+from fastapi import APIRouter, BackgroundTasks, Depends
+import logging
 
+log = logging.getLogger(__name__)
 router = APIRouter(prefix="/films", tags=["Films"])
 
 
@@ -13,10 +15,15 @@ def show_films():
     return storage.get_films()
 
 
-@router.post(
-    "/create/",
-    response_model=FilmsCreate | None,
-    status_code=status.HTTP_201_CREATED,
-)
-def create_film(film_create: FilmsCreate):
+def spam_log():
+    log.info("spam log")
+
+
+@router.post("/", response_model=FilmsCreate, status_code=status.HTTP_201_CREATED)
+def create_film(
+    film_create: FilmsCreate,
+    background_tasks: BackgroundTasks,
+    _=Depends(spam_log),
+):
+    background_tasks.add_task(storage.save_state)
     return storage.create_film(create_films=film_create)
