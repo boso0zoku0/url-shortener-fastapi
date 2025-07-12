88d80527244290b3aae84098f28eef09a3a8854b
BOSOZOKU <matveygrishayev@mail.ru>
2025-07-12 18:18:14 +0300

    create API_TOKENS

--- url-shortener/core/config.py
+++ url-shortener/core/config.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import logging
 
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 SHORT_URLS_STORAGE_FILEPATH = BASE_DIR / "short_urls.json"
 
@@ -9,3 +10,6 @@ LOG_LEVEL = logging.INFO
 LOG_FORMAT: str = (
     "[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s"
 )
+
+
+API_TOKENS = frozenset({"eD83xIM7oJMn-WmuiDJPJQ", "HDk4YMx_Lu54ETlueqxTdw"})
