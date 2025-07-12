38c7701fc153b06eb2b7a407644193e4d48c1650
BOSOZOKU <matveygrishayev@mail.ru>
2025-07-12 16:09:50 +0300

    add depends(save_storage_state)

--- url-shortener/api/api_v1/films/views/list_router.py
+++ url-shortener/api/api_v1/films/views/list_router.py
@@ -5,25 +5,25 @@ from schemas.films import FilmsRead, FilmsCreate
 
 from fastapi import APIRouter, BackgroundTasks, Depends
 import logging
+from api.api_v1.dependencies import save_storage_state
 
 log = logging.getLogger(__name__)
-router = APIRouter(prefix="/films", tags=["Films"])
+router = APIRouter(
+    prefix="/films", tags=["Films"], dependencies=[Depends(save_storage_state)]
+)
+
+
+# def write_notification(email: str, message=""):
+#     with open("log.txt", mode="w") as email_file:
+#         content = f"notification for {email}: {message}"
+#         email_file.write(content)
 
 
 @router.get("/", response_model=list[FilmsRead])
-def show_films():
+def show_films(_=Depends(save_storage_state)):
     return storage.get_films()
 
 
-def spam_log():
-    log.info("spam log")
-
-
 @router.post("/", response_model=FilmsCreate, status_code=status.HTTP_201_CREATED)
-def create_film(
-    film_create: FilmsCreate,
-    background_tasks: BackgroundTasks,
-    _=Depends(spam_log),
-):
-    background_tasks.add_task(storage.save_state)
+def create_film(film_create: FilmsCreate, _=Depends(save_storage_state)):
     return storage.create_film(create_films=film_create)
