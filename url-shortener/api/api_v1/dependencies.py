954ae6c74cce887fd3c7fcb4c85d1971d9d58cfc
BOSOZOKU <matveygrishayev@mail.ru>
2025-07-12 13:59:20 +0300

    change strings from api.api_v1.films.crud import storage from api.api_v1.films.crud import storage as film_storage

--- url-shortener/api/api_v1/dependencies.py
+++ url-shortener/api/api_v1/dependencies.py
@@ -2,7 +2,7 @@ from schemas.short_url import ShortUrl
 from schemas.films import FilmsRead
 from fastapi import HTTPException, status, Depends
 from api.api_v1.short_urls.crud import storage
-from api.api_v1.films.crud import storage
+from api.api_v1.films.crud import storage as film_storage
 
 
 def prefetch_url(slug: str):
@@ -15,15 +15,16 @@ def prefetch_url(slug: str):
 
 
 def prefetch_url_film(slug: str):
-    url: FilmsRead | None = storage.get_by_slug(slug=slug)
+    url: FilmsRead | None = film_storage.get_by_slug(slug=slug)
     if url:
         return url
     raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND, detail=f"URL {slug!r} not found"
+        status_code=status.HTTP_404_NOT_FOUND, detail=f"Film {slug!r} not found"
     )
 
 
-def get_film_by_slug_exc(slug=Depends(storage.get_by_slug)):
+#
+def get_film_by_slug_exc(slug: str = Depends(film_storage.get_by_slug)):
     if slug is None:
-        raise HTTPException(status_code=404, detail=f"Film {slug} not found")
+        raise HTTPException(status_code=404, detail=f"Slug by Film: {slug} not found")
     return slug
