85e805260b3198a73ece4ce880b278788157e468
BOSOZOKU <matveygrishayev@mail.ru>
2025-07-15 17:10:26 +0300

    add redis

---
+++ url-shortener/stuff.py
@@ -0,0 +1,29 @@
+from redis import Redis
+from core import config
+
+redis = Redis(
+    host=config.REDIS_HOST,
+    port=config.REDIS_PORT,
+    db=config.REDIS_DB,
+    decode_responses=True,
+)
+
+
+def main():
+    print(redis.ping())
+    redis.set("name", "zoku")
+    redis.set("age", "22")
+    redis.set("car", "bmw")
+    print(redis.get("name"))
+    print(redis.get("age"))
+    redis.getdel("car")
+    print(redis.get("car"))
+    print(f"Name: {redis.get('name')}. Age: {redis.get('age')}")
+    redis.delete("name")
+    redis.delete("age")
+    print(redis.get("name"))
+    print(redis.get("age"))
+
+
+if __name__ == "__main__":
+    main()
