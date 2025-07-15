5562eb569d7f9c47cedb8fa95bcf5d4c613fd407
BOSOZOKU <matveygrishayev@mail.ru>
2025-07-15 17:11:03 +0300

    update main file

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from starlette.requests import Request
+
+from api import router as api_router
+import logging
+from core import config
+from app_lifespan import lifespan
+
+log = logging.basicConfig(format=config.LOG_FORMAT, level=config.LOG_LEVEL)
+app = FastAPI(lifespan=lifespan)
+app.include_router(router=api_router)
