93098028b869e28cb242bc736814eb298888c658
BOSOZOKU <matveygrishayev@mail.ru>
2025-07-18 15:46:49 +0300

    created invalid views redis

--- url-shortener/api/api_v1/short_urls/views/list_router.py
+++ url-shortener/api/api_v1/short_urls/views/list_router.py
@@ -4,6 +4,7 @@ from starlette import status
 from api.api_v1.short_urls.crud import storage
 from schemas.short_url import ShortUrl, ShortUrlCreate, ShortUrlRead
 from fastapi import Depends
+from api.api_v1.redis_db import redis as db_redis
 from api.api_v1.dependencies import (
     save_storage_state,
     api_token_or_basic_auth_for_unsafe_methods,
@@ -31,6 +32,11 @@ router = APIRouter(
 )
 
 
+@router.post("/add-token")
+def generate_and_save_token(token: str):
+    return db_redis.generate_and_save_token(token)
+
+
 @router.get("/read-urls", response_model=list[ShortUrl])
 def read_short_urls_list() -> list[ShortUrlRead]:
     return storage.get()
@@ -44,3 +50,8 @@ def create_short_url(short_url: ShortUrlCreate):
 @router.get("/search", response_model=ShortUrlRead)
 def search_url(slug: str):
     return storage.get_by_slug(slug=slug)
+
+
+@router.post("/add-token", status_code=status.HTTP_201_CREATED)
+def add_token(token):
+    return db_redis.add_token(token)
