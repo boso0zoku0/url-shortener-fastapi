7e460733632bc2329a17fc670a5a53403772ec19
BOSOZOKU <matveygrishayev@mail.ru>
2025-07-21 18:39:11 +0300

    create save_short_url

--- url-shortener/api/api_v1/short_urls/crud.py
+++ url-shortener/api/api_v1/short_urls/crud.py
@@ -41,6 +41,13 @@ class ShortUrlsStorage(BaseModel):
         self.slug_by_short_urls.update(data.slug_by_short_urls)
         log.warning("Recovered data from storage file.")
 
+    def save_short_url(self, short_url: ShortUrl) -> None:
+        redis.hset(
+            name=config.REDIS_SHORT_URLS_HASH_NAME,
+            key=short_url.slug,
+            value=short_url.model_dump_json(),
+        )
+
     def get(self):
         return [
             ShortUrlRead.model_validate_json(value)
@@ -54,11 +61,7 @@ class ShortUrlsStorage(BaseModel):
 
     def create(self, short_url_create: ShortUrlCreate) -> ShortUrl:
         short_url = ShortUrl(**short_url_create.model_dump())
-        redis.hset(
-            name=config.REDIS_SHORT_URLS_HASH_NAME,
-            key=short_url.slug,
-            value=short_url.model_dump_json(),
-        )
+        self.save_short_url(short_url)
         log.warning("Created short url %s", short_url)
         return short_url
 
@@ -72,6 +75,7 @@ class ShortUrlsStorage(BaseModel):
     def update(self, short_url: ShortUrl, short_url_update: ShortUrlUpdate) -> ShortUrl:
         for field_name, value in short_url_update:
             setattr(short_url, field_name, value)
+        self.save_short_url(short_url)
         log.info("Updated short url %s", short_url)
         return short_url
 
@@ -82,6 +86,7 @@ class ShortUrlsStorage(BaseModel):
             exclude_unset=True
         ).items():
             setattr(short_url, field_name, value)
+        self.save_short_url(short_url)
         log.info("Updated short url %s", short_url)
         return short_url
 
