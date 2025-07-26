2e3f1fa0d84ee25e4d4af08a7ec384f9a8bec398
BOSOZOKU <matveygrishayev@mail.ru>
2025-07-26 17:00:21 +0300

    fix schemas film: added target_url . created class FilmsSubTestTestCase

--- url-shortener/tests/test_schemas/test_schemas_films.py
+++ url-shortener/tests/test_schemas/test_schemas_films.py
@@ -6,6 +6,7 @@ class FilmsTestCase(TestCase):
     def test_film_can_be_created_from_create_scheme(self) -> None:
         film_in = FilmsCreate(
             name="Matrix",
+            target_url="https://example.com",
             description="Is film Matrix",
             year_release=2000,
         )
@@ -19,6 +20,7 @@ class FilmsTestCase(TestCase):
     def test_film_can_be_created_from_update_scheme(self) -> None:
         film_in = FilmsUpdate(
             name="Matrix",
+            target_url="https://example.com",
             description="Is film Matrix",
             year_release=2000,
         )
@@ -32,6 +34,7 @@ class FilmsTestCase(TestCase):
     def test_empty_movie_can_be_created_from_partial_update_scheme(self) -> None:
         film_in = FilmsUpdatePartial(
             name=None,
+            target_url=None,
             description=None,
             year_release=None,
         )
@@ -45,10 +48,48 @@ class FilmsTestCase(TestCase):
     ) -> None:
         film_in = FilmsUpdatePartial(
             name="Matrix",
+            target_url="https://example.com",
             description=None,
             year_release=1999,
         )
         film = FilmsUpdatePartial(**film_in.model_dump())
         self.assertEqual(film.name, film_in.name)
+        self.assertEqual(film.target_url, film_in.target_url)
         self.assertEqual(film.description, film_in.description)
         self.assertEqual(film.year_release, film_in.year_release)
+
+
+class FilmsSubTestTestCase(TestCase):
+
+    def test_films_create_accepts_different_urls(self) -> None:
+        urls = [
+            "https://example.com",
+            "https://www.example.com",
+            "https://example",
+        ]
+
+        for url in urls:
+            with self.subTest(url=url, msg=f"added url: {url}"):
+                film_in = FilmsCreate(
+                    name="test_film",
+                    target_url=url,
+                    description="Is new film",
+                    year_release=1999,
+                )
+                self.assertEqual(
+                    url.rstrip("/"),
+                    film_in.model_dump(mode="json")["target_url"].rstrip("/"),
+                )
+
+    def test_films_update_accepts_different_urls(self) -> None:
+        names = ["Matrix", "maTriX", "Clan @Soprano", " dw цв"]
+
+        for name in names:
+            with self.subTest(name=names, msg=f"new name: {name}"):
+                film_in = FilmsUpdate(
+                    name=name,
+                    target_url="https://kinopoisk.com",
+                    description="Is new film",
+                    year_release=1999,
+                )
+                self.assertEqual(name, film_in.model_dump(mode="json")["name"])
