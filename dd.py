fae8177555fdb458bf60716bf68f249ce164fcef
BOSOZOKU <matveygrishayev@mail.ru>
2025-07-29 17:12:16 +0300

    garbage

---
+++ dd.py
@@ -0,0 +1,38 @@
+# dictlist = [
+#     {
+#         "type": "string_too_long",
+#         "loc": ["description"],
+#         "msg": "String should have at most 30 characters",
+#         "input": "This string contains more than thirty alphabetic characters.",
+#         "ctx": {"max_length": 30},
+#         "url": "https://errors.pydantic.dev/2.11/v/string_too_long",
+#     }
+# ]
+#
+# data = dictlist["input"]
+#
+# print(data)
+
+import os
+import pprint
+
+# os.environ["name"] = "zoku"
+
+# print(os.environ["name"])
+# num = os.getenv("name")
+#
+# print(num)
+
+
+# print(os.getcwd())
+# os.chdir(cd..)
+# print(os.getcwd())
+
+# mask = 0o777
+#
+#
+# umask = os.umask(mask)
+#
+#
+# print("Current umask:", mask)
+# print("Previous umask:", umask)
