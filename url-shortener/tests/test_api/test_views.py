722bb1dd446eba5f5d17c4d9a1772ba64a9e47d9
BOSOZOKU <matveygrishayev@mail.ru>
2025-08-03 18:58:41 +0300

    fixture

--- url-shortener/tests/test_api/test_views.py
+++ url-shortener/tests/test_api/test_views.py
@@ -1,23 +1,20 @@
 import pytest
 from fastapi.testclient import TestClient
-from main import app
 from fastapi import status
 
-client = TestClient(app)
+from tests.test_api.conftest import client
 
 
-def test_main_views() -> None:
-    name = "Bob"
-    query = {"name": name}
-    response = client.get("/", params=query)
+def test_main_views(client: TestClient) -> None:
+    response = client.get("/")
     assert response.status_code == status.HTTP_200_OK, response.text
     response_data = response.json()
-    assert "message" in response_data
-    assert response_data["message"] == f"Hello {name}", response_data
+    expected_message = "Hello World"
+    assert response_data["message"] == expected_message
 
 
 @pytest.mark.parametrize("name", ["John", "!@#$%&", "John Snow", ""])
-def test_main_custom_views(name: str) -> None:
+def test_main_custom_views(name: str, client: TestClient) -> None:
     query = {"name": name}
     response = client.get("/", params=query)
     assert response.status_code == status.HTTP_200_OK, response.text
