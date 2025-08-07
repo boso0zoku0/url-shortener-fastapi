781c4c67b303af223296d4fb1becb0436ccd598d
BOSOZOKU <matveygrishayev@mail.ru>
2025-08-07 12:55:22 +0300

    rename files from routers to views

--- url-shortener/api/api_v1/short_urls/views/__init__.py
+++ url-shortener/api/api_v1/short_urls/views/__init__.py
@@ -1,6 +1,6 @@
 __all__ = ("router",)
 
-from .details_router import router as details_router
-from .list_router import router
+from .details_views import router as details_router
+from .list_views import router
 
 router.include_router(router=details_router)
