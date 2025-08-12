02fc25c29eee223ceefa7501b19205e3ae0c7c3c
BOSOZOKU <matveygrishayev@mail.ru>
2025-08-12 16:12:16 +0300

    add checking_env

--- url-shortener/tests/conftest.py
+++ url-shortener/tests/conftest.py
@@ -2,5 +2,9 @@ from os import getenv
 
 import pytest
 
-if getenv("TESTING") != "1":
-    raise pytest.exit("Environment variable TESTING must be 1")
+
+@pytest.fixture(scope="session", autouse=True)
+def checking_env():
+    if getenv("TESTING") != "1":
+        pytest.exit("This test must be run in test mode")
+        
