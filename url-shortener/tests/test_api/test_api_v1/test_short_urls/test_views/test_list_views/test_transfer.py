c34af2f4c669c985147d664608acabfe3c7698ed
BOSOZOKU <matveygrishayev@mail.ru>
2025-08-12 20:01:00 +0300

    Added a demonstrative example of using the xfail marker

---
+++ url-shortener/tests/test_api/test_api_v1/test_short_urls/test_views/test_list_views/test_transfer.py
@@ -0,0 +1,15 @@
+import pytest
+from starlette import status
+from starlette.testclient import TestClient
+from main import app
+
+
+@pytest.mark.xfail(
+    reason="not implemented yet",
+    raises=ValueError,
+)
+@pytest.mark.apitest
+def test_transfer(auth_client: TestClient) -> None:
+    url = app.url_path_for("transfer_short_url")
+    response = auth_client.post(url=url)
+    assert response.status_code == status.HTTP_200_OK
